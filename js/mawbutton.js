69576e27a11ea661479140650bc6319d848b555e
Michael Chen <yuhua1226@gmail.com>
2014-07-14 03:13:07 +0800

    Implement it as jQuery Plugin.

--- js/mawbutton.js
+++ js/mawbutton.js
@@ -1,30 +1,50 @@
-$(function(){
-	//add effect element for each.
-	$(".mawbutton").each(function(){
-		var effect = $(this).attr("data-effect");
-		$(this).append('<div class="mawbutton-'+effect+'"></div>');
-	});
-	//bind click event
-	$(".mawbutton").on('click', function(e) {
-		e.preventDefault();	
-		//fetch click position ,effect & w,h
-		var posX = $(this).offset().left,
-			posY = $(this).offset().top;
-		var effect = $(this).attr("data-effect");
-		var w = $(this).width(),
-			h = $(this).height();
-		var targetX= e.pageX - posX;
-		var targetY= e.pageY - posY;
-		//Animate Start
-		$effectElem = $(this).find(".mawbutton-"+effect);
-		$effectElem.addClass("mawbutton-stop");
-		$effectElem.css("top", targetY  ).css("left", targetX).css("width",h * 3 ).css("height",h * 3).css("margin-left", -h * 3 / 2 ).css("margin-top", -h * 3 / 2);
-		$effectElem.removeClass("mawbutton-stop");
+(function($){
 
-		//Animate End
-		setTimeout(function(){
-			$effectElem.addClass("mawbutton-stop");
-			$effectElem.attr("style","");
-		},300);
-	});
-});
+	$.fn.mawbutton = function (options) {
+		var settings = $.extend({
+			effect : "ripple",
+			scale : 3,
+			speed : 300		// ms
+		}, options);
+
+		return this.each(function() {
+			var $this = $(this);
+			$this.addClass('mawbutton')
+			.append('<div class="mawbutton-'+settings.effect+'"></div>')
+			.on('click', function(e) {		//bind click event
+				e.preventDefault();
+
+				// Fetch click position and size
+				var posX = $this.offset().left,
+					posY = $this.offset().top;
+				var w = $this.width(),
+					h = $this.height();
+				var targetX= e.pageX - posX;
+				var targetY= e.pageY - posY;
+				var ratio = settings.scale / 2;
+
+				//Animate Start
+				$effectElem = $this.find(".mawbutton-"+settings.effect);
+				$effectElem.addClass("mawbutton-stop")
+							.css({
+								"top" : targetY,
+								"left" : targetX,
+								"width" : h*settings.scale,
+								"height" : h*settings.scale,
+								"margin-left" : -h * ratio,
+								"margin-top" : -h * ratio,
+								"transition-duration" : settings.speed+"ms",
+								"-webkit-transition-duration" : settings.speed+"ms",
+								"-moz-transition-duration" : settings.speed+"ms",
+								"-o-transition-duration" : settings.speed+"ms"
+							});
+				$effectElem.removeClass("mawbutton-stop");
+
+				//Animate End
+				setTimeout(function(){
+					$effectElem.addClass("mawbutton-stop").attr("style","");
+				}, settings.speed);
+			});
+		});
+	}
+}(jQuery));
