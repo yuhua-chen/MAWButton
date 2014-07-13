25f9afad14969dadf060ea96a4a66c536ca55cd0
Will Huang <ravewill123@gmail.com>
2014-07-14 01:02:31 +0800

    fixed crif problem & remove style.css & update mawbutton stop animation className.

--- js/mawbutton.js
+++ js/mawbutton.js
@@ -10,22 +10,23 @@ $(function(){
 	$(".mawbutton").on('click', function(e) {
 		e.preventDefault();	
 
-		//fetch click position & effect	
+		//fetch click position ,effect & w,h
 		var posX = $(this).offset().left,
 			posY = $(this).offset().top;
 		var effect = $(this).attr("data-effect");
+		var w = $(this).width(),
+			h = $(this).height();
 		console.log("Target:"+ ( e.pageX - posX ) + "," + ( e.pageY - posY ));
-
 		//Anime Start
 		$effectElem = $(this).find(".mawbutton-"+effect);
 
-		$effectElem.addClass("stopAnime");
-		$effectElem.css("top",posY).css("left",posY);
-		$effectElem.removeClass("stopAnime");
+		$effectElem.addClass("mawbutton-stop");
+		$effectElem.css("top",posY).css("left",posY).css("width",w).css("height",h);
+		$effectElem.removeClass("mawbutton-stop");
 
 		//Anime End
 		setTimeout(function(){
-			$effectElem.addClass("stopAnime");
+			$effectElem.addClass("mawbutton-stop");
 			$effectElem.attr("style","");
 		},250);
 	});
