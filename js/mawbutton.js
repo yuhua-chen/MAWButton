faaf4a1280e2153ea231d504a3797f8e940cf777
Michael Chen <michaelchen@kkbox.com>
2014-07-16 11:24:26 +0800

    Fix coding style and update README

--- js/mawbutton.js
+++ js/mawbutton.js
@@ -4,16 +4,19 @@
 		var settings = $.extend({
 			effect : "ripple",
 			scale : 3,
-			speed : 300		// ms
+			speed : 300,					// ms
+			transitionEnd : function(){}	// callback when transition ends.
 		}, options);
 
 		return this.each(function() {
 			var $this = $(this);
+			var supportEvent = ('ontouchstart' in window ) ? 'touchstart':'click';
+
 			$this.addClass('mawbutton')
-			
-			.on('click', function(e) {		//bind click event
+			.on(supportEvent, function(e) {		//bind touch/click event
 				e.preventDefault();
 				$this.append('<div class="mawbutton-'+settings.effect+'" ></div>');
+				
 				// Fetch click position and size
 				var posX = $this.offset().left,
 					posY = $this.offset().top;
@@ -21,17 +24,23 @@
 					h = $this.height();
 				var targetX= e.pageX - posX;
 				var targetY= e.pageY - posY;
+
+				//Fix target position
+				if(!targetX || !targetY){
+					 targetX = e.originalEvent.touches[0].pageX;
+					 targetY = e.originalEvent.touches[0].pageY;
+				}
+
 				var ratio = settings.scale / 2;    							
 
 				//Animate Start
 				$effectElem = $this.children(':last');
-
 				$effectElem.addClass("mawbutton-stop")
 							.css({
 								"top" : targetY,
 								"left" : targetX,
-								"width" : h*settings.scale,
-								"height" : h*settings.scale,
+								"width" : h * settings.scale,
+								"height" : h * settings.scale,
 								"margin-left" : -h * ratio,
 								"margin-top" : -h * ratio,
 								"transition-duration" : settings.speed+"ms",
@@ -51,6 +60,7 @@
 					});
 					setTimeout(function(){
 						$this.find(".mawbutton-"+settings.effect).first().remove();
+						settings.transitionEnd.call(this);
 					},settings.speed);
 				}, settings.speed);
 			});
